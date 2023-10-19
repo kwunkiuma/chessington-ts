5bf584f63eab257c0133781db9c654124a59faea
Sleepyuser <36637730+SleepyUser@users.noreply.github.com>
2023-10-19 10:04:38 +0100

    Rook can get available moves along its row and column
    (cherry picked from commit a77ad053c0b0b872a8598dd3187df7a558b2e81b)

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,24 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableSquares: Square[] =[];
+        const square = board.findPiece(this);
+        for (let i = square.row+1; i <= 7; i++)
+        {
+            availableSquares.push(Square.at(i, square.col));
+        }
+        for (let i = square.row-1; i >= 0; i--)
+        {
+            availableSquares.push(Square.at(i, square.col));
+        }
+        for (let i = square.col-1; i >= 0; i--)
+        {
+            availableSquares.push(Square.at(square.row, i));
+        }
+        for (let i = square.col+1; i <= 7; i++)
+        {
+            availableSquares.push(Square.at(square.row, i));
+        }
+        return availableSquares;
     }
 }
