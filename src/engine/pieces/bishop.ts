8bdb8ac166106d909e651b3f9cab92dd77d04bc1
Sleepyuser <36637730+SleepyUser@users.noreply.github.com>
2023-10-19 10:46:42 +0100

    Bishops can move diagonally in all four directions

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,24 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableSquares: Square[] =[];
+        const square = board.findPiece(this);
+        for (let i = 1; Math.max(square.col +i, square.row + i) <= 7; i++)
+        {
+            availableSquares.push(Square.at( square.row + i,square.col + i));
+        }
+        for (let i = 1; Math.min( square.row  -i, square.col -i,) >= 0; i++)
+        {
+            availableSquares.push(Square.at(square.row - i, square.col - i));
+        }
+        for (let i = 1; square.row  + i <= 7 &&  square.col - i >= 0; i++)
+        {
+            availableSquares.push(Square.at(square.row + i, square.col - i));
+        }
+        for (let i = 1; square.row  - i >= 0 &&  square.col +i <= 7; i++)
+        {
+            availableSquares.push(Square.at(square.row - i, square.col + i));
+        }
+        return availableSquares;
     }
 }
